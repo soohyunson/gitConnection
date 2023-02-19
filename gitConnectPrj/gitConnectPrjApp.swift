d2d021efab75ca47438aaf5294e92add01a657c8
ssh <ssoohyun126@gmail.com>
2023-02-19 14:49:38 +0900

    Initial Commit

---
+++ gitConnectPrj/gitConnectPrjApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitConnectPrjApp.swift
+//  gitConnectPrj
+//
+//  Created by 손수현 on 2023/02/19.
+//
+
+import SwiftUI
+
+@main
+struct gitConnectPrjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
